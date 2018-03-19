397807ba18215810aa30eae986c1f1c12899eadf
SanchelliosProg <vasyania2009@gmail.com>
2018-03-19 09:07:47 +0300

    CLOSE FORM BUTTON const is added

--- FlexDatabases/Helpers/ProductsSelectors.cs
+++ FlexDatabases/Helpers/ProductsSelectors.cs
@@ -8,5 +8,6 @@ namespace FlexDatabases.Helpers {
     public static class ProductsSelectors {
         public const String MAIN_REQUEST_BUTTON = "div.customdemo-btn div.demo-btn-cont a";
         public const String REQUEST_DEMO_FORM = "form.chronoform";
+        public const String CLOSE_FORM_BUTTON = "div#close-demo:nth-child(1)";
     }
 }
